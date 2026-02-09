e971a3ad708bed72c96384de5b871aab576b2f84
SuperStik <daviswood290@gmail.com>
2026-02-09 10:02:29 -0800

    Reformatting

--- Assets/Scripts/Managers/ApplicationManager.cs
+++ Assets/Scripts/Managers/ApplicationManager.cs
@@ -1,12 +1,13 @@
-﻿namespace Managers {
+﻿using UnityEditor;
+
+namespace Managers {
 	public static class ApplicationManager {
 		// Already Declared as Singleton from "GenericHelpers"
-		public static void QuitApplication()
-		{
+		public static void QuitApplication() {
 #if UNITY_EDITOR
-			UnityEditor.EditorApplication.isPlaying = false;
+			EditorApplication.isPlaying = false;
 #else
-                Application.Quit();
+			Application.Quit();
 #endif
 		}
 	}
