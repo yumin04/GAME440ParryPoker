1287166dcf1d593b365cbbe9f2801c802634275a
yumin04 <yumin@chapman.edu>
2026-02-10 14:32:27 -0800

    Changing name from ApplicationManager to EndApplication so that it would not be calling anything else

---
+++ Assets/Scripts/Managers/ApplicationEssentials/EndApplication.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+
+public static class EndApplication {
+	// Already Declared as Singleton from "GenericHelpers"
+	public static void QuitApplication() {
+		#if UNITY_EDITOR
+				EditorApplication.isPlaying = false;
+		#else
+				Application.Quit();
+		#endif
+	}
+}
