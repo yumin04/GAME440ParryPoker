4733c59be90038ba8cba39a399bcc8d826a5b130
SuperStik <daviswood290@gmail.com>
2026-02-15 12:44:28 -0800

    Use good binding address for host, and allow specifying address for client

--- Assets/Scripts/Managers/HostClientManager.cs
+++ Assets/Scripts/Managers/HostClientManager.cs
@@ -1,53 +1,51 @@
-﻿using UnityEngine;
-using Unity.Netcode;
+﻿using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
 
-public class HostClientManager : Singleton<HostClientManager>
-{
-    // private bool clientConnected = false;
-    protected override void Awake()
-    {
-        base.Awake();
-        Debug.Log("NetworkConnectionManager Awake");
-    }
-    public void StartHost()
-    {
-        NetworkManager.Singleton.StartHost();
-        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-    }
-    
-    public void EndHost()
-    {
-        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
-    }
+namespace Managers {
+	public class HostClientManager : Singleton<HostClientManager> {
+		// private bool clientConnected = false;
+		protected override void Awake() {
+			base.Awake();
+			Debug.Log("NetworkConnectionManager Awake");
+		}
 
-    public void StartClient()
-    {
-        NetworkManager.Singleton.StartClient();
-        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-    }
+		public void StartHost() {
+			// we want to bind to all identities for the best chance of succeeding
+			NetworkChangeAddress("0.0.0.0");
+			NetworkManager.Singleton.StartHost();
+			NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+		}
 
-    public void EndClient()
-    {
-        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
-    }
-    
-    private void OnClientConnected(ulong clientId)
-    {
-        if (NetworkManager.Singleton.IsHost)
-        {
-            if (clientId != NetworkManager.Singleton.LocalClientId)
-            {
-                Debug.Log("Host: Client connected");
-                SceneLoader.Instance.LoadRoundScene();
-            }
-        }
-        else if (NetworkManager.Singleton.IsClient)
-        {
-            if (clientId == NetworkManager.Singleton.LocalClientId)
-            {
-                Debug.Log("Client: Connected to host");
-            }
-        }
-    }
+		public void EndHost() {
+			NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+		}
 
+		public void StartClient(string address = "127.0.0.1") {
+			// bind to a given address, default to standard address because Unity no like localhost
+			NetworkChangeAddress(address);
+			NetworkManager.Singleton.StartClient();
+			NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+		}
+
+		public void EndClient() {
+			NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+		}
+
+		private static void OnClientConnected(ulong clientId) {
+			if (NetworkManager.Singleton.IsHost) {
+				if (clientId == NetworkManager.Singleton.LocalClientId) return;
+				Debug.Log("Host: Client connected");
+				SceneLoader.Instance.LoadRoundScene();
+			}
+			else if (NetworkManager.Singleton.IsClient) {
+				if (clientId == NetworkManager.Singleton.LocalClientId) Debug.Log("Client: Connected to host");
+			}
+		}
+
+		// Unity doesn't like making this easy for us, so let's just use a private static method
+		private static void NetworkChangeAddress(string address) {
+			NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = address;
+		}
+	}
 }
