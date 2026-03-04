a889a5e3bf30794395915b0e3fbe20a9f2cdc632
SuperStik <daviswood290@gmail.com>
2026-03-04 13:29:38 -0800

    Try setting card texture on init and update

--- Assets/Scripts/GeneralScripts/Card/Card.cs
+++ Assets/Scripts/GeneralScripts/Card/Card.cs
@@ -1,71 +1,69 @@
-﻿using System;
-using SOFile;
+﻿using SOFile;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Card : NetworkBehaviour
-{
-    public NetworkVariable<int> cardId = new NetworkVariable<int>();
-    
-    [SerializeField] private CardDataSO cardData;
-    
-    public void Init(int cardId)
-    {
-        this.cardId.Value = cardId;
-    }
+namespace GeneralScripts.Card {
+	public class Card : NetworkBehaviour {
+		public NetworkVariable<int> cardId = new();
 
-    
-    public void OnEnable()
-    {
-        GameEvents.HideAllInstantiatedCards += HideCard;
-        GameEvents.DestroyAllInstantiatedCards += DestroyCard;
-        cardId.OnValueChanged += OnCardDataChanged;
-        GameEvents.OnKeepClicked += TryDespawn;
-    }
+		[SerializeField] private CardDataSO cardData;
+		private static readonly int Slice = Shader.PropertyToID("_Slice");
 
-    
-    
-    public void OnDisable()
-    {
-        GameEvents.HideAllInstantiatedCards -= HideCard;
-        GameEvents.DestroyAllInstantiatedCards -= DestroyCard;
-        cardId.OnValueChanged -= OnCardDataChanged;
-        GameEvents.OnKeepClicked -= TryDespawn;
-    }
-    
-    // TODO: Refactor this so Card does not need to know this maybe?
-    private void OnCardDataChanged(int previousValue, int newValue)
-    {
-        cardData = CardManager.Instance.GetCardByID(newValue);
-        // TODO: This is where we change the texture of the card.
-    }    
-    
-    private void TryDespawn()
-    {
-        // if (!IsOwner) return;
-        DespawnCardRPC();
-    }
-    
-    [Rpc(SendTo.Server)]
-    private void DespawnCardRPC(RpcParams rpcParams = default)
-    {
-        NetworkObject.Despawn(true);
-    }
+		public void Init(int id) {
+			cardId.Value = id;
+			UpdateTexture();
+		}
 
-    private void HideCard()
-    {
-        // Or move to a different place
-        gameObject.SetActive(false);
-    }
 
-    private void DestroyCard()
-    {
-        Destroy(gameObject);
-    }
-    private void ShowCard()
-    {
-        gameObject.SetActive(true);
-    }
-    
+		public void OnEnable() {
+			GameEvents.HideAllInstantiatedCards += HideCard;
+			GameEvents.DestroyAllInstantiatedCards += DestroyCard;
+			cardId.OnValueChanged += OnCardDataChanged;
+			GameEvents.OnKeepClicked += TryDespawn;
+		}
 
+		public void OnDisable() {
+			GameEvents.HideAllInstantiatedCards -= HideCard;
+			GameEvents.DestroyAllInstantiatedCards -= DestroyCard;
+			cardId.OnValueChanged -= OnCardDataChanged;
+			GameEvents.OnKeepClicked -= TryDespawn;
+		}
+
+		// TODO: Refactor this so Card does not need to know this maybe?
+		private void OnCardDataChanged(int previousValue, int newValue) {
+			cardData = CardManager.Instance.GetCardByID(newValue);
+			UpdateTexture();
+		}
+
+		private void TryDespawn() {
+			// if (!IsOwner) return;
+			DespawnCardRPC();
+		}
+
+		[Rpc(SendTo.Server)]
+		private void DespawnCardRPC(RpcParams rpcParams = default) {
+			NetworkObject.Despawn();
+		}
+
+		private void HideCard() {
+			// Or move to a different place
+			gameObject.SetActive(false);
+		}
+
+		private void DestroyCard() {
+			Destroy(gameObject);
+		}
+
+		private void ShowCard() {
+			gameObject.SetActive(true);
+		}
+
+		private void UpdateTexture() {
+			var propertyBlock = new MaterialPropertyBlock();
+			// hack for managing the suit values, some changes will have to be made I'm sure
+			propertyBlock.SetFloat(Slice, (int)cardData.cardSymbol * 4 + cardData.cardNumber);
+
+			GetComponent<MeshRenderer>().SetPropertyBlock(propertyBlock);
+		}
+	}
 }
