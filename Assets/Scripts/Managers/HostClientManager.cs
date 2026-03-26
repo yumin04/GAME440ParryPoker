aec4e168b4dd0688fcf48010bc532d737fe3a7ef
SuperStik <daviswood290@gmail.com>
2026-03-26 13:53:00 -0700

    Set IP address on client start

--- Assets/Scripts/Managers/HostClientManager.cs
+++ Assets/Scripts/Managers/HostClientManager.cs
@@ -1,100 +1,87 @@
-﻿using System;
+﻿using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
-using Unity.Netcode;
 using UnityEngine.SceneManagement;
 
-public class HostClientManager : Singleton<HostClientManager>
-{
-    // private bool clientConnected = false;
-    protected override void Awake()
-    {
-        base.Awake();
-        Debug.Log("NetworkConnectionManager Awake");
-    }
+namespace Managers {
+	public class HostClientManager : Singleton<HostClientManager> {
+		// private bool clientConnected = false;
+		protected override void Awake() {
+			base.Awake();
+			Debug.Log("NetworkConnectionManager Awake");
+		}
 
-    private void OnEnable()
-    {
-        GameEvents.OnLoadResultScene += EndHost;
-        
-    }
-    private void OnDisable()
-    {
-        GameEvents.OnLoadResultScene -= EndHost;
-    }
-    public void StartHost()
-    {
-        NetworkManager.Singleton.StartHost();
-        NetworkManager.Singleton.SceneManager.OnLoadComplete += OnSceneLoaded;
-        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-    }
+		private void OnEnable() {
+			GameEvents.OnLoadResultScene += EndHost;
+		}
 
-    
-    public void StartClient()
-    {
-        NetworkManager.Singleton.StartClient();
-        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
-    }
-    
-    public void EndHost()
-    {
-        NetworkManager.Singleton.SceneManager.OnLoadComplete -= OnSceneLoaded;
-        Debug.Log("NetworkConnectionManager EndHost");
-        EndNetworkSession();
-    }
+		private void OnDisable() {
+			GameEvents.OnLoadResultScene -= EndHost;
+		}
 
-    public void EndClient() 
-    {
-        Debug.Log("NetworkConnectionManager EndClient");
-        EndNetworkSession();
-    }
-    
-    #region private methods
-    private void EndNetworkSession()
-    {
-        if (!NetworkManager.Singleton) return;
-
-        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
-
-        if (NetworkManager.Singleton.IsListening)
-        {
-            NetworkManager.Singleton.Shutdown();
-        }
-    }   
-    private void OnClientConnected(ulong clientId)
-    {
-        if (NetworkManager.Singleton.IsHost)
-        {
-            if (clientId != NetworkManager.Singleton.LocalClientId)
-            {
-                Debug.Log("Host: Client connected");
-                SceneLoader.Instance.LoadRoundScene();
-            }
-        }
-        else if (NetworkManager.Singleton.IsClient)
-        {
-            if (clientId == NetworkManager.Singleton.LocalClientId)
-            {
-                Debug.Log("Client: Connected to host");
-            }
-        }
-    }
-    private void OnSceneLoaded(
-        ulong clientId,
-        string sceneName,
-        LoadSceneMode mode)
-    {
-        if (!NetworkManager.Singleton.IsServer) return;
-
-        if (sceneName == "RoundScene" &&
-            clientId == NetworkManager.Singleton.LocalClientId)
-        {
-            GameInitializer.Instance.SpawnGame();
-        }
-    }
-    #endregion
-    
+		public void StartHost() {
+			NetworkManager.Singleton.StartHost();
+			NetworkManager.Singleton.SceneManager.OnLoadComplete += OnSceneLoaded;
+			NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+		}
 
 
+		public void StartClient(string ipAddress = "127.0.0.1") {
+			SetIPAddress(ipAddress);
+			NetworkManager.Singleton.StartClient();
+			NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+		}
 
+		public void EndHost() {
+			NetworkManager.Singleton.SceneManager.OnLoadComplete -= OnSceneLoaded;
+			Debug.Log("NetworkConnectionManager EndHost");
+			EndNetworkSession();
+		}
 
+		public void EndClient() {
+			Debug.Log("NetworkConnectionManager EndClient");
+			EndNetworkSession();
+		}
+
+		#region private methods
+
+		private void EndNetworkSession() {
+			if (!NetworkManager.Singleton) return;
+
+			NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+
+			if (NetworkManager.Singleton.IsListening) NetworkManager.Singleton.Shutdown();
+		}
+
+		private void OnClientConnected(ulong clientId) {
+			if (NetworkManager.Singleton.IsHost) {
+				if (clientId != NetworkManager.Singleton.LocalClientId) {
+					Debug.Log("Host: Client connected");
+					SceneLoader.Instance.LoadRoundScene();
+				}
+			}
+			else if (NetworkManager.Singleton.IsClient) {
+				if (clientId == NetworkManager.Singleton.LocalClientId) Debug.Log("Client: Connected to host");
+			}
+		}
+
+		private void OnSceneLoaded(
+			ulong clientId,
+			string sceneName,
+			LoadSceneMode mode) {
+			if (!NetworkManager.Singleton.IsServer) return;
+
+			if (sceneName == "RoundScene" &&
+			    clientId == NetworkManager.Singleton.LocalClientId)
+				GameInitializer.Instance.SpawnGame();
+		}
+
+		private void SetIPAddress(string ipAddress = "127.0.0.1") {
+			// I hate this code
+			var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+			if (transport) transport.ConnectionData.Address = ipAddress;
+		}
+
+		#endregion
+	}
 }
