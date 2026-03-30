a530acc63556b4ef404b2b772ad350ecac19c540
yumin04 <yumin@chapman.edu>
2026-03-29 23:45:29 -0700

    #1 Trailer Video End

---
+++ Assets/Scripts/Trailer/PlayerObjectSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerObjectSwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    
+    [Header("Poses")]
+    public GameObject cardHold;
+    public GameObject handLay;
+    public GameObject cardCatch;
+    public GameObject frogDeath;
+    public GameObject frogEvilSmile;
+    public GameObject frogGrab;
+    public GameObject frogHit;
+    public GameObject frogIdle;
+    public GameObject frogShoot;
+    public GameObject frogVictory;
+    
+    // 핵심 함수
+    public GameObject SwapPose(GameObject posePrefab, Transform targetTransform)
+    {
+        GameObject obj = Instantiate(posePrefab, targetTransform.position,  targetTransform.rotation);
+
+        // obj.transform.position = targetTransform.position;
+        // obj.transform.rotation = targetTransform.rotation;
+        // obj.transform.localScale = targetTransform.localScale;
+        return obj;
+    }
+
+    public void DeactivatePlayer1()
+    {
+        player1.SetActive(false);
+    }
+    public void DeactivatePlayer2()
+    {
+        player2.SetActive(false);
+    }
+    public void ActivatePlayer1()
+    {
+        player1.SetActive(true);
+    }
+    public void ActivatePlayer2()
+    {
+        player2.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Trailer/TrailerObjectInstantiator.cs
@@ -0,0 +1,65 @@
+﻿using SOFile;
+using UnityEngine;
+
+public class TrailerObjectInstantiator : MonoBehaviour
+{
+    [SerializeField] private GameObject slotMachine;
+    [SerializeField] private GameObject crossHair;
+    
+    
+    
+    private readonly float[] xPositions = { -4.5f, -3f, -1.5f, 0f, 1.5f };
+
+    private readonly float[] zPositions = { 0f, 2f };
+
+    private const float yPosition = -0.3f;
+
+    private const float xMinBoundary = -5.5f;
+    private const float xMaxBoundary = 2f;
+
+    private const float zMinBoundary = -0.5f;
+    private const float zMaxBoundary = 2.5f;
+    
+    [SerializeField] private GameObject Card;
+    [SerializeField] private CardRepository cardRepository;
+    
+    public void InstantiateRoundCardsByID(int[] ids)
+    {
+        CardDataSO[] cards = new CardDataSO[ids.Length];
+
+        for (int i = 0; i < ids.Length; i++)
+        {
+            cards[i] = cardRepository.GetCardByID(ids[i]);
+        }
+
+        InstantiateRoundCards(cards);
+    }
+    private void InstantiateRoundCards(CardDataSO[] roundCards)
+    {
+        int index = 0;
+        foreach (float x in xPositions)
+        {
+            foreach (float z in zPositions)
+            {
+                Vector3 spawnPos = new Vector3(x, yPosition, z);
+                Instantiate(Card, spawnPos, Quaternion.identity);
+                index++;
+            }
+        }
+    }
+
+    public void InstantiateSubRoundCard(int cardId)
+    {
+        Vector3 randomPosition = GenerateRandomXZPosition();
+        CardDataSO subRoundCard = cardRepository.GetCardByID(cardId);
+        // Generate Random Position
+        Instantiate(Card, randomPosition, Quaternion.identity);
+    }    
+    private Vector3 GenerateRandomXZPosition()
+    {
+        float x = Random.Range(xMinBoundary, xMaxBoundary);
+        float z = Random.Range(zMinBoundary, zMaxBoundary);
+
+        return new Vector3(x, yPosition, z);
+    }
+}
